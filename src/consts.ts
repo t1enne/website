1b6a0bd202e0182b4ac3a6a1ff703e7f86a52d22
t1enne <nasir.taov@gmail.com>
2024-08-06 13:24:35 +0200

    feat (i18n): added en/it translations

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,80 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Nasir Taov personal website";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Nasir Taov's Personal Website";
+export const SITE_DESCRIPTION = "Welcome!";
+
+export const LANGS = {
+  en: "English",
+  it: "Italiano",
+} as const;
+
+export const defaultLang = "en";
+
+export const ui = {
+  en: {
+    "about.title": "About",
+    "about.content":
+      "Experienced Software Dev with a robust background in frontend development and a strong track record in delivering complex projects. Adept at guiding small teams and driving innovation within e-commerce platforms, web applications and ERP systems. Demonstrated the ability to lead small teams, and effectively communicate with both stakeholders and colleagues, fostering an environment of continuous learning and improvement. Proficient in creating strategic technical roadmaps and ensuring the alignment of technology with business goals. A fan of OSS and Linux",
+    "work.title": "Work Experience",
+    "education.title": "Education",
+    "langs.title": "Languages",
+
+    "location.padua": "Padua, Italy",
+    "location.perugia": "Perugia, Italy",
+    "location.solomeo": "Solomeo, Italy",
+
+    "title.fullstack": "Full Stack Developer",
+    "title.boutique": "Boutique Coordinator",
+
+    "work.rt.desc":
+      "Worked on development and enhanments of ERP systems for large enterprise clients, mainly leveraging Node. Working as the company's lead developer for a new front-end for ATMs across Europe for a leading Italian bank. Oversaw architectural planning and provided strategic direction for the project. Managed a small development team (1 to 4 people), conducted thorough code reviews, and ensured high-quality deliverables. Acted as the primary technical liaison with the client's project manager, facilitating clear and effective communication. Additionally, optimized build processes, containerized some of our apps with Docker, performed SysAdmin tasks, and established Jenkins CI/CD pipelines to streamline development workflows.",
+    "work.cortek.desc":
+      "I played a pivotal role in orchestrating e-commerce infrastructure, primarily leveraging the Shopify platform. Also worked on tools to help with day to day operations. Additionally, I spearheaded content creation for e-commerce platforms and expanded sales channels to include platforms such as Amazon and Meta. My contributions directly contributed to the growth and success of the company's online presence.",
+    "work.bc.desc":
+      "During my tenure at Brunello Cucinelli, I excelled in various roles, primarily as a Boutique Coordinator. In this capacity, I provided guided tours of the company's production facilities to high profile international clients (Forbes 100) in Italian, English, Russian and French. Additionally, I specialized in Made to Measure services and offered comprehensive customer consultation and service throughout the sales process. I was proficient in utilizing AS400/Salesforce systems. Furthermore, I implemented visual merchandising guidelines for window displays and shelves and managed restocking activities, participating in order processing during sales campaigns in collaboration with the Store Manager.",
+
+    "edu.bd.title": "Bachelor's Degree in Linguistic Mediation",
+
+    "langs.en": "English",
+    "langs.fr": "French",
+    "langs.it": "Italian",
+    "langs.ru": "Russian",
+    "langs.lvl.bi": "Bilingual",
+    "langs.lvl.vf": "Very fluent",
+    "langs.lvl.f": "Fluent",
+    "dates.july": "Jul",
+    "dates.jan": "Jan",
+    "dates.march": "Mar",
+  },
+  it: {
+    "about.title": "Chi sono",
+    "about.content":
+      "Sviluppatore software esperto con una solida esperienza nello sviluppo frontend e una comprovata esperienza nella consegna di progetti complessi. Abile nel guidare piccoli team e promuovere l'innovazione all'interno delle piattaforme di e-commerce, applicazioni web e sistemi ERP. Dimostrata capacità di guidare piccoli team e comunicare efficacemente con stakeholder e colleghi, favorendo un ambiente di apprendimento e miglioramento continuo. Competente nella creazione di roadmap tecniche strategiche e nell'allineare la tecnologia con gli obiettivi aziendali. Appassionato di OSS e Linux.",
+    "work.title": "Esperienza Lavorativa",
+    "education.title": "Formazione",
+    "langs.title": "Lingue",
+    "location.padua": "Padova (PD)",
+    "location.perugia": "Perugia (PG)",
+    "location.solomeo": "Solomeo (PG)",
+    "title.fullstack": "Sviluppatore Full Stack",
+    "title.boutique": "Coordinatore di Boutique",
+    "work.rt.desc":
+      "Lavorato allo sviluppo e miglioramento dei sistemi ERP per clienti aziendali di grandi dimensioni, principalmente utilizzando Node. Lavoro come sviluppatore principale dell'azienda per un nuovo front-end per ATM in tutta Europa (600.000 macchine) per una grande banca italiana. Supervisionato la pianificazione architettonica e fornito direzione strategica per il progetto. Gestito un piccolo team di sviluppo (da 1 a 4 persone), condotto PR e garantito consegne di alta qualità. Agito come principale referente tecnico con il project manager del cliente, facilitando una comunicazione chiara ed efficace. Inoltre, ottimizzato i processi di build, containerizzato alcune delle nostre applicazioni con Docker, eseguito compiti di amministrazione di sistema e stabilito pipeline CI/CD con Jenkins per ottimizzare i flussi di lavoro di sviluppo.",
+    "work.cortek.desc":
+      "Ho svolto un ruolo fondamentale nell'orchestrare l'infrastruttura di e-commerce, utilizzando principalmente la piattaforma Shopify. Ho anche lavorato su strumenti per aiutare con le operazioni quotidiane. Inoltre, ho guidato la creazione di contenuti per piattaforme di e-commerce e ampliato i canali di vendita includendo piattaforme come Amazon e Meta. I miei contributi hanno direttamente contribuito alla crescita e al successo della presenza online dell'azienda.",
+    "work.bc.desc":
+      "Durante la mia esperienza presso Brunello Cucinelli, ho eccelso in vari ruoli, principalmente come Coordinatore di Boutique. In questo ruolo, ho fornito visite guidate degli impianti di produzione dell'azienda a clienti internazionali di alto profilo (Forbes 100) in italiano, inglese, russo e francese. Inoltre, mi sono specializzato nei servizi su misura e ho offerto consulenza e servizio clienti completi durante tutto il processo di vendita. Ero competente nell'utilizzo dei sistemi AS400/Salesforce. Inoltre, ho implementato linee guida di visual merchandising per le vetrine e gli scaffali e gestito le attività di rifornimento, partecipando all'elaborazione degli ordini durante le campagne di vendita in collaborazione con il responsabile del negozio.",
+    "edu.bd.title": "Laurea in Mediazione Linguistica",
+    "langs.en": "Inglese",
+    "langs.fr": "Francese",
+    "langs.it": "Italiano",
+    "langs.ru": "Russo",
+    "langs.lvl.bi": "Bilingue",
+    "langs.lvl.vf": "Molto fluente",
+    "langs.lvl.f": "Fluente",
+    "dates.july": "Lug",
+    "dates.jan": "Gen",
+    "dates.march": "Mar",
+  },
+} as const;
