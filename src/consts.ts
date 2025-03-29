9638669deca3238902262f611b3f7afdc4d057d3
t1enne <nasir.taov@gmail.com>
2025-03-29 10:04:24 +0100

    feat: desc update

--- src/consts.ts
+++ src/consts.ts
@@ -14,14 +14,11 @@ export const defaultLang = "en";
 export const ui = {
   en: {
     "about.title": "About",
-    "about.content":
-      `The most important thing to know about me is that I love what I do, and I put passion in whatever I do. 
+    "about.content": `
+Developed a custom UI framework powering 600.000 devices. 
+Working on full stack Node.js applications with governmental institutions and banks.
 
-I like my CLI, remote machines and maximizing productivity on my machine. 
-More formally, I've developed a UI framework for a leading Italian bank,
-led the development of CRM's and worked with governmental entities for a variety of software projects.
-
-Familiar with Next/Angular and more broadly with the whole TS ecosystem.`,
+Passionate about OSS, Linux, testing and functional programming.`,
     "work.title": "Work Experience",
     "education.title": "Education",
     "langs.title": "Languages",
@@ -34,15 +31,19 @@ Familiar with Next/Angular and more broadly with the whole TS ecosystem.`,
     "title.boutique": "Boutique Coordinator",
 
     "work.rt.desc":
-      `Worked on development and enhanments of ERP systems for large enterprise clients, mainly leveraging Node.
-Led the development of a new front-end for ATMs across Europe for a leading Italian bank.
+      `Led the development of a new FE for ATMs across Europe for a leading Italian bank.
 Oversaw architectural planning and provided strategic direction for the project.
-Managed a small dev team, with code reviews and training session.
-Acted as the primary technical liaison with the client's project manager, facilitating clear and effective communication. Additionally, optimized build processes, containerized some of our apps with Docker, performed SysAdmin tasks, and established Jenkins CI/CD pipelines to streamline development workflows.`,
+Acted as the primary technical liaison with the client's PM, facilitating clear and effective communication. 
+Also worked on development for large enterprise clients, mainly leveraging Node.
+Led small teams (1 to 4 people) during different phases of development.
+Other tasks:
+- Optimizing build processes/packaging 
+- Containerization via Docker 
+- Setting up on premise Jenkins CI/CD pipelines`,
     "work.cortek.desc":
       `Had a pivotal role in orchestrating e-commerce infrastructure, primarily leveraging the Shopify platform.
-Also worked on tools to help with day to day operations.
-Spearheaded content creation for e-commerce platforms and expanded sales channels to include platforms such as Amazon and Meta`,
+Also worked on tools to help with day to day operations (moving .csv to SQL, FE for retail stores, integrations with cash registers).
+Led content creation for e-commerce platforms (Amazon and Meta)`,
     "work.bc.desc":
       `During my tenure at Brunello Cucinelli, I excelled in various roles, primarily as a Boutique Coordinator.
 In this capacity, I provided guided tours of the company's production facilities to high profile international clients (Forbes 100) in Italian, English, Russian and French.
@@ -65,13 +66,11 @@ Furthermore, I implemented visual merchandising guidelines for window displays a
   it: {
     "about.title": "Chi sono",
     "about.content": `
-La cosa piu' importante da sapere su di me e' che amo cio' che faccio e metto passione per portare a casa il miglior risultato possibile.
+Autore di framework UI custom per grande banca italiana, usato su 600.000 ATM. 
+Sviluppo applicazione full stack Node.js per istituzioni. 
 
-Mi piace la CLI, le macchine remote e massimizzare la produttivita' nel mio ambiente.
-Piu' formalmente, ho sviluppato un framework UI per una banca italiana,
-guidato sviluppi di CRM e lavorato con entita' governative su progetti di grandi dimensioni.
-
-Familiarizzato con Next/Angular e in generale con l'ecosistema TS.`,
+Mi piace l'OSS, Linux, il testing e la programmazione funzionale.
+`,
     "work.title": "Esperienza Lavorativa",
     "education.title": "Formazione",
     "langs.title": "Lingue",
@@ -81,9 +80,20 @@ Familiarizzato con Next/Angular e in generale con l'ecosistema TS.`,
     "title.fullstack": "Sviluppatore Full Stack",
     "title.boutique": "Coordinatore di Boutique",
     "work.rt.desc":
-      "Lavorato allo sviluppo e miglioramento dei sistemi ERP per clienti aziendali di grandi dimensioni, principalmente utilizzando Node. Lavoro come sviluppatore principale dell'azienda per un nuovo front-end per ATM in tutta Europa (600.000 macchine) per una grande banca italiana. Supervisionato la pianificazione architettonica e fornito direzione strategica per il progetto. Gestito un piccolo team di sviluppo (4 persone), con training delle risorse assegnate. Agito come principale referente tecnico con il project manager del cliente, facilitando una comunicazione chiara ed efficace. Inoltre, ottimizzato i processi di build, containerizzato alcune delle nostre applicazioni con Docker, eseguito compiti di amministrazione di sistema e stabilito pipeline CI/CD con Jenkins per ottimizzare i flussi di lavoro di sviluppo.",
-    "work.cortek.desc":
-      "Ho svolto un ruolo fondamentale nell'orchestrare l'infrastruttura di e-commerce, utilizzando principalmente la piattaforma Shopify. Ho anche lavorato su strumenti per aiutare con le operazioni quotidiane. Inoltre, ho guidato la creazione di contenuti per piattaforme di e-commerce e ampliato i canali di vendita includendo piattaforme come Amazon e Meta. I miei contributi hanno direttamente contribuito alla crescita e al successo della presenza online dell'azienda.",
+      `Guidato lo sviluppo di framework FE per ATM in tutta Europa.
+Supervisionato architettura e strategia per il progetto.
+Primo punto di contatto con il PM del cliente.
+Lavorato anche per grandi aziende, principalmente con Node.
+Guidato piccoli team (da 1 a 4 persone) durante diverse fasi di vita dei progetti.
+Altri task:
+- Ottimizzare packaging e build
+- Containerizzazione con Docker
+- Setup di pipeline Jenkins CI/CD on-premise
+		`,
+    "work.cortek.desc": `
+Orchestrazione dell'infrastruttura di e-commerce, principalmente tramite Shopify. 
+Lavorato su strumenti per aiutare con le operazioni quotidiane (.csv to SQL, FE per negozi, integrazioni casse).
+Guidato la creazione di contenuti per piattaforme di e-commerce (Amazon e Meta)`,
     "work.bc.desc":
       "Durante la mia esperienza presso Brunello Cucinelli, ho eccelso in vari ruoli, principalmente come Coordinatore di Boutique. In questo ruolo, ho fornito visite guidate degli impianti di produzione dell'azienda a clienti internazionali di alto profilo (Forbes 100) in italiano, inglese, russo e francese. Inoltre, mi sono specializzato nei servizi su misura e ho offerto consulenza e servizio clienti completi durante tutto il processo di vendita. Ero competente nell'utilizzo dei sistemi AS400/Salesforce. Inoltre, ho implementato linee guida di visual merchandising per le vetrine e gli scaffali e gestito le attività di rifornimento, partecipando all'elaborazione degli ordini durante le campagne di vendita in collaborazione con il responsabile del negozio.",
     "edu.bd.title": "Laurea in Mediazione Linguistica",
