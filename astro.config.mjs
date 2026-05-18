52ad3e8719566a728767e278e2a71d812966181d
nasir.taov <nasir.taov@raintonic.com>
2026-05-18 14:50:04 +0200

    fix: redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
+const SITE_URL = process.env.URL || process.env.SITE_URL || "https://example.com";
+
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.SITE_URL || "https://example.com",
+  site: SITE_URL,
   integrations: [sitemap(), tailwind()],
   i18n: {
     defaultLocale: "en",
