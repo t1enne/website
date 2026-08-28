56345c26b7a1e135c316eb207de3c12415f0a9cb
t1enne <nasir.taov@gmail.com>
2026-08-28 19:09:39 +0200

    feat: add prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,21 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 
-const SITE_URL = process.env.URL || process.env.SITE_URL || "https://example.com";
+const SITE_URL =
+  process.env.URL || process.env.SITE_URL || "https://example.com";
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
-  integrations: [sitemap(), tailwind(), alpinejs({ entrypoint: "/src/entrypoint" })],
+  integrations: [
+    sitemap(),
+    tailwind(),
+    alpinejs({ entrypoint: "/src/entrypoint" }),
+  ],
+  prefetch: {
+    prefetchAll: true, // auto-prefetch every internal <a>
+    defaultStrategy: "hover", // fire when the link is hovered
+  },
   i18n: {
     defaultLocale: "en",
     locales: ["en", "it", "fr"],
@@ -18,4 +27,3 @@ export default defineConfig({
     },
   },
 });
-
